4155ade4044120dcd28a28d921ecfbcf873b17d5
pangyin2 <155937386+pangyin2@users.noreply.github.com>
2024-04-03 22:16:01 -0400

    add lintr

---
+++ scripts/lintr.R
@@ -0,0 +1,12 @@
+# Install the lintr package
+#install.packages("lintr")
+
+# Load the lintr library
+library(lintr)
+
+# Lint through all the scripts in the current directory
+lint_dir(path = ".")
+
+#Remember to fix errors manually!
+
+
